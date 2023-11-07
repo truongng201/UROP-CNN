59ee5149b6482c7dede696162efc60a8d6e1c59f
Nemo <truongng201@gmail.com>
2023-11-07 10:06:42 +0700

    update data

--- data.py
+++ data.py
@@ -6,15 +6,23 @@ class Dataset:
         self.transform = transforms.Compose(
             [transforms.ToTensor(),
              transforms.Normalize((0.5,), (0.5,))])
-        self.train_dataset = torchvision.datasets.CIFAR10(
+        self.train_dataset = torchvision.datasets.MNIST(
             root='./data',
             train=True,
             download=True,
             transform=self.transform
         )
-        self.test_dataset = torchvision.datasets.CIFAR10(
+        self.test_dataset = torchvision.datasets.MNIST(
             root='./data',
             train=False,
             download=True,
             transform=self.transform
         )
+   
+    def get_train_dataset(self):
+        return self.train_dataset
+    
+    def get_test_dataset(self):
+        return self.test_dataset
+
+Dataset()
