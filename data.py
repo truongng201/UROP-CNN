db4be8aeb3abc7055e4335a787d720ee71961819
Nemo <truongng201@gmail.com>
2023-11-07 11:42:20 +0700

    update data

--- data.py
+++ data.py
@@ -1,29 +1,60 @@
 from argparse import ArgumentParser
+import builtins
+
 import torchvision
 import torchvision.transforms as transforms
+import torch.utils.data.DataLoader as DataLoader
+
 
 class Dataset:
     def __init__(self):
         self.transform = transforms.Compose(
             [transforms.ToTensor(),
              transforms.Normalize((0.5,), (0.5,))])
+        self.train_dataset = None
+        self.test_dataset = None
+    
 
-    def download(self, data_dir):
-        torchvision.datasets.CIFAR10(
-            root=data_dir,
-            train=True,
-            download=True,
-            transform=self.transform
+    def __load_dataset(self, data_dir):
+        self.train_dataset = torchvision.datasets.CIFAR10(
+                root=data_dir, 
+                train=True,
+                download=True,
+                transform=self.transform
         )
-        torchvision.datasets.CIFAR10(
-            root=data_dir,
-            train=False,
-            download=True,
-            transform=self.transform
+        self.test_dataset= torchvision.datasets.CIFAR10(
+                root=data_dir, 
+                train=False,
+                download=True,
+                transform=self.transform
         )
 
 
-parser = ArgumentParser()
-parser.add_argument("--data-dir", type=str, default="../data")
-args = parser.parse_args()
-Dataset().download(args.data_dir)
+    def __data_loader(self, bach_size):
+        train_loader = torch.utils.data.DataLoader(
+            dataset=self.train_dataset,
+            batch_size=bath_size,
+            shuffle=True
+        )
+        test_loader = torch.utils.data.DataLoader(
+            dataset=self.test_dataset,
+            batch_size=batch_size,
+            shuffle=False
+        )
+        builtins.train_loader = train_loader
+        builtins.test_loader = test_loader
+
+
+    def execute(self, data_dir, batch_size):
+        self.load_dataset(data_dir)
+        self.data_loader(batch_size)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='../data')
+    parser.add_argument('--batch_size', type=int, default=64)
+    args = parser.parse_args()
+    dataset = Dataset()
+    dataset.execute(args.data_dir, args.batch_size)
+    print('Done!')
--- train.py
+++ train.py
@@ -1,5 +1,30 @@
 from model import Net
-from data import Dataset
+epochs = 100
+# Create the network
+net = Net()
 
-Dataset()
+# Create the optimizer
+optimizer = optim.SGD(net.parameters(), lr=0.01)
 
+# Create the loss function
+criterion = nn.CrossEntropyLoss()
+
+# 
+
+# Training loop
+net.train()
+for epoch in range(epochs):
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = net(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print("Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                epoch,
+                batch_idx * len(data),
+                len(train_loader.dataset),
+                100.0 * batch_idx / len(train_loader),
+                loss.item()
+            ))
