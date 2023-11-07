89daa1e7d2d692d5ce41792f46456f4d1a293702
Nemo <truongng201@gmail.com>
2023-11-07 10:50:09 +0700

    update data

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import parser
 import torchvision
 import torchvision.transforms as transforms
 
@@ -7,19 +8,22 @@ class Dataset:
             [transforms.ToTensor(),
              transforms.Normalize((0.5,), (0.5,))])
 
-    def download(self):
-        torchvision.datasets.CIFAR10(
-            root='../data',
+    def download(self, data_dir):
+        torchvision.sets.CIFAR10(
+            root=data_dir,
             train=True,
             download=True,
             transform=self.transform
         )
-        torchvision.datasets.CIFAR10(
-            root='../data',
+        torchvision.sets.CIFAR10(
+            root=data_dir,
             train=False,
             download=True,
             transform=self.transform
         )
 
 
-Dataset().download()
+parser = ArguemntParser()
+parser.add_argument("--data-dir", type=str, default="../data")
+args = parser.parse_args()
+Dataset().download(args.data_dir)
