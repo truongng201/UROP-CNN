c3b82b8146b0953ba47e3ae569cdc9ded568c7f5
Nemo <truongng201@gmail.com>
2023-11-07 10:14:51 +0700

    update data.py

--- data.py
+++ data.py
@@ -6,23 +6,20 @@ class Dataset:
         self.transform = transforms.Compose(
             [transforms.ToTensor(),
              transforms.Normalize((0.5,), (0.5,))])
-        self.train_dataset = torchvision.datasets.MNIST(
+
+    def download(self):
+        torchvision.datasets.MNIST(
             root='./data',
             train=True,
             download=True,
             transform=self.transform
         )
-        self.test_dataset = torchvision.datasets.MNIST(
+        torchvision.datasets.MNIST(
             root='./data',
             train=False,
             download=True,
             transform=self.transform
         )
-   
-    def get_train_dataset(self):
-        return self.train_dataset
-    
-    def get_test_dataset(self):
-        return self.test_dataset
 
-Dataset()
+
+Dataset().download()
