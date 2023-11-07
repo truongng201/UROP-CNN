33b23b1b2cb0c6f035817ebbb6970a91398d9131
Nemo <truongng201@gmail.com>
2023-11-07 13:40:40 +0700

    minor update

--- model.py
+++ model.py
@@ -7,13 +7,13 @@ import torch.optim as optim
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(28*28, 100)
+        self.fc1 = nn.Linear(256*256*3, 100)
         self.fc2 = nn.Linear(100, 10)
         self.softmax = nn.Softmax(dim=1)
 
 
     def forward(self, x):
-        x = x.view(-1, 28*28)
+        x = x.view(-1, 256*256*3)
         x = F.relu(self.fc1(x))
         x = self.softmax(self.fc2(x))
         return x
