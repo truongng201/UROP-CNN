12fc02dd129fda4e401c7df546f83b3fbeb0823c
Nemo <truongng201@gmail.com>
2023-11-07 13:36:01 +0700

    minor update

---
+++ setup.sh
@@ -0,0 +1,12 @@
+DATA_DIR = 'data'
+
+if [ ! -d $DATA_DIR ]; then
+  mkdir $DATA_DIR
+  cd /content/data
+  wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+  tar -xzvf cifar-10-python.tar.gz
+  rm -f cifar-10-python.tar.gz
+else
+  echo "Directory $DATA_DIR already exists."
+fi
+
